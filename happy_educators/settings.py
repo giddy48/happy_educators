9217851cb9fdb2d69900d3f84eb4c56855571c71
Gedion Mutavi <gedionmutavi817@gmail.com>
2026-04-08 17:14:14 +0300

    fixed migrations and production setup

--- happy_educators/settings.py
+++ happy_educators/settings.py
@@ -10,10 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-change-this-in-production'
 
 DEBUG = False
-
-ALLOWED_HOSTS = ["*"]
-
-
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".onrender.com"]
 # ---------------------------------------------------
 # APPLICATIONS
 # ---------------------------------------------------
